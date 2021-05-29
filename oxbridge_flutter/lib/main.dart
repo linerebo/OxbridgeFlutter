9bf1d05eb52bf0220e8ee656146652978d89c08d
linerebo <55693839+linerebo@users.noreply.github.com>
2021-05-29 14:12:02 +0200

    FirstCommit

--- oxbridge_flutter/lib/main.dart
+++ oxbridge_flutter/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'user.dart';
 import 'package:oxbridge_flutter/route_generator.dart';
 
+//main
 void main() {
   runApp(MaterialApp(
     title: 'Oxbridge',
