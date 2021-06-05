a9b82c2e306d7c81746d820b3a6a6c1d4b0f3a28
linerebo <55693839+linerebo@users.noreply.github.com>
2021-06-05 15:34:41 +0200

    updates on methode create new user

--- oxbridge_flutter/lib/http_service.dart
+++ oxbridge_flutter/lib/http_service.dart
@@ -1,13 +1,13 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:html';
+//import 'dart:html';
 import 'package:http/http.dart';
 import 'user_model.dart';
 import 'event_model.dart';
 
 class HttpService {
-  //final String userURL = "http://10.0.2.2:3000/users/register"; //emulator
-  final String userURL = "http://localhost:3000";
+  //final String userURL = 'http://10.0.2.2:3000/users/register';
+  final String userURL = 'http://localhost:3000/users/register';
   //final String userURL = "http://192.168.178.46:3000/users/register";
 
   final String eventsURL = "http://10.0.2.2:3000/events";
@@ -16,7 +16,9 @@ class HttpService {
       String emailUsername, String password) async {
     final Response response = await post(Uri.parse(userURL),
         headers: <String, String>{
-          'Content-Type': 'application/json;charset=UTF-8'
+          "Accept": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Content-Type": "application/json;charset=UTF-8",
         },
         body: jsonEncode(<String, String>{
           "firstname": firstname,
@@ -24,11 +26,14 @@ class HttpService {
           "emailUsername": emailUsername,
           "password": password
         }));
-    if (response.statusCode == 200) {
-      // print(response.body);
-      return User.fromJson(json.decode(response.body));
+    if (response.statusCode == 201) {
+      User newUser = new User(
+          firstname: firstname,
+          lastname: lastname,
+          emailUsername: emailUsername,
+          password: password);
+      return newUser;
     } else {
-      // print('error');
       throw Exception(response.body);
     }
   }
@@ -40,10 +45,10 @@ class HttpService {
       List<dynamic> body = jsonDecode(response.body);
 
       List<Event> events = body
-        .map(
-          (dynamic item) => Event.fromJson(item),
-        )
-        .toList();
+          .map(
+            (dynamic item) => Event.fromJson(item),
+          )
+          .toList();
 
       return events;
     } else {
@@ -51,4 +56,3 @@ class HttpService {
     }
   }
 }
-
